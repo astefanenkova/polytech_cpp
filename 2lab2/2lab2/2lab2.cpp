3fac692067f16f078e361f0810170c39d5b416f9
Анастасия Стефаненкова <stefanenkova.anastasia.w@gmail.com>
2022-09-16 13:23:33 +0300

    Продолжение второй лабораторной

---
+++ 2lab2/2lab2/2lab2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int x, y;
+    char z;
+    
+    cout << "Введите число x: ";
+    cin >> x;
+    cout << "Введите число y: ";
+    cin >> y;
+    cout << "Введите знак: +, -, * или / ";
+    cin >> z; 
+    
+     /*if (z == '+') cout << (x + y);
+    else if (z == '-') cout << (x - y);
+    else if (z == '*') cout << (x * y);
+    else if (z == '/') cout << (x / y);
+    else cout << "Некорректный ввод"; */
+ 
+    switch (z)
+    {
+    case('+'):
+        cout << (x + y);
+        break;
+    case ('-'):
+        cout << (x - y);
+        break;
+    case ('*'):
+        cout << (x * y);
+        break;
+    case('/'):
+        cout << (x / y);
+        break;
+    default:
+        std::cout << "некорректный ввод";
+        break;
+    }
+    
+
+}
+
