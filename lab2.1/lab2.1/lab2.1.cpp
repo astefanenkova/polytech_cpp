ff02d65eb082aae79d9b4b4f35868206685d8658
Анастасия Стефаненкова <stefanenkova.anastasia.w@gmail.com>
2022-09-16 14:06:57 +0300

    Лабораторная 2.1

---
+++ lab2.1/lab2.1/lab2.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+    int x;
+    int n, s;
+    s = 0;
+
+    cout << "Введите число: ";
+    do
+    {
+        cin >> x;
+
+        if (x>0)
+        {
+            s += x;
+        }
+        
+    } 
+    while (x > 0);
+
+    cout << "Сумма чисел = " << s << endl;
+
+    int A[10];
+
+    for (int i = 0; i < 10; i++) 
+    {
+            cout << "[" << i + 1 << "]" << ": ";
+            cin >> A[i];
+    }
+
+    for (int i = 0; i < 10; i++) 
+    {
+        cout << i+1 <<"ое число массива = "<< A[i] << endl;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (A[i] % 2 == 0)
+        {
+            cout << A[i] << "" << endl;
+        }
+
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (A[i] % 2 != 0)
+        {
+            cout << A[i] << "" << endl;
+        }
+
+    }
+}
